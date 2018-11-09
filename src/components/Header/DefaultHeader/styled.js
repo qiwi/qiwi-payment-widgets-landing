b315ba3b72ccf02feca93e11baa53cf621bf01cb
Evgenii <e.kukhta@qiwi.com>
2018-11-09 21:27:18 +0300

    Header rectangles

--- src/components/Header/DefaultHeader/styled.js
+++ src/components/Header/DefaultHeader/styled.js
@@ -6,30 +6,43 @@ export const StyledHeader = styled(CommonHeader)`
 `;
 
 export const RectanglesHolder = styled.div`
-    padding-left: 40%;
-    height: 100%;
+    margin-left: 35%;
+    position: relative;
+    display: inline-block;
+    box-sizing: border-box;
+    height: inherit;
     width: 100%;
-    
-    &:nth-child(1) {
-        margin-top: 0px;
-    }
-    
-    &:nth-child(2) {
-        margin-top: 225px;
-    }
-    
-    &:nth-child(3) {
-        margin-top: 540px;
-    }
-    
-    &:nth-child(4) {
-        margin-top: 700px;
-    }
 `;
 
 export const Rectangle = styled.div`
+    width: 1000px;
+    height: 1000px;
+    
+    transform-origin: bottom left; 
+    position: absolute;
+    
     transform: rotate(-315deg);
     border-radius: 100px;
     box-shadow: 20px 12px 94px 0 #f2440a;
-    background-image: radial-gradient(circle at 61% 32%, #fc882b, #f76b1c);    
+    background-image: radial-gradient(circle at 32% 61%, #f76b1c, #fc882b);
+    
+    &:nth-child(4) {
+        top: -400px;
+        left: 240px;
+    }
+    
+    &:nth-child(3) {
+        top: -620px;
+        left: 120px;
+    }
+    
+    &:nth-child(2) {
+        top: -840px;
+        left: 0;
+    }
+    
+    &:nth-child(1) {
+        top: -1060px;
+        left: -120px;
+    }  
 `
